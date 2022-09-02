98706d014aa116238b359cd4ec11e7d7f13b8e91
rugamoto <apetoshi.rugamoto@gmail.com>
2022-09-02 11:48:35 +0100

    feat: approval verify, split into interfaces and errors

--- contracts/SocketV2Verifier.sol
+++ contracts/SocketV2Verifier.sol
@@ -2,97 +2,85 @@
 pragma solidity >=0.8.4;
 
 import "hardhat/console.sol";
+import "./interfaces/ISocketRegistry.sol";
+import "./helpers/errors.sol";
 
 contract SocketV2Verifier {
-    /// @param id route id of middleware to be used
-    /// @param optionalNativeAmount is the amount of native asset that the route requires
-    /// @param inputToken token address which will be swapped to BridgeRequest inputToken
-    /// @param data to be used by middleware
-    struct MiddlewareRequest {
-        uint256 id;
-        uint256 optionalNativeAmount;
-        address inputToken;
-        bytes data;
-    }
-
-    /// @param id route id of bridge to be used
-    /// @param optionalNativeAmount optinal native amount, to be used when bridge needs native token along with ERC20
-    /// @param inputToken token addresss which will be bridged
-    /// @param data bridgeData to be used by bridge
-    struct BridgeRequest {
-        uint256 id;
-        uint256 optionalNativeAmount;
-        address inputToken;
-        bytes data;
-    }
-
-    /// @param receiverAddress Recipient address to recieve funds on destination chain
-    /// @param toChainId Destination ChainId
-    /// @param amount amount to be swapped if middlewareId is 0  it will be
-    /// the amount to be bridged
-    /// @param middlewareRequest middleware Requestdata
-    /// @param bridgeRequest bridge request data
-    struct UserRequest {
-        address receiverAddress;
-        uint256 toChainId;
-        uint256 amount;
-        MiddlewareRequest middlewareRequest;
-        BridgeRequest bridgeRequest;
-    }
-
     /// @notice Decode the socket v2 calldata
     /// @param data Socket V2 outboundTransferTo call data
     /// @return userRequest socket UserRequest
-    function decodeCallData(bytes calldata data) internal pure returns (UserRequest memory userRequest) {
-        (userRequest) = abi.decode(data[4:], (UserRequest));
+    function decodeCallData(bytes calldata data)
+        internal
+        pure
+        returns (ISocketRegistry.UserRequest memory userRequest)
+    {
+        (userRequest) = abi.decode(data[4:], (ISocketRegistry.UserRequest));
+    }
+
+    /// @notice Decode erc20 approval call
+    /// @param data ERC20 apporval calldata
+    /// @return spender erc20 spender
+    /// @return amount erc20 amount
+    function decodeApprovalData(bytes calldata data) internal pure returns (address spender, uint256 amount) {
+        (spender, amount) = abi.decode(data[4:], (address, uint256));
     }
 
     /// @notice Verify the bridge request
     /// @param request Bridge request
     /// @param expectedRequest Bridge request that is expected
-    function verifyBridgeRequest(BridgeRequest calldata request, BridgeRequest calldata expectedRequest) public pure {
-        require(request.id == expectedRequest.id, "bridge id invalid");
+    function verifyBridgeRequest(
+        ISocketRegistry.BridgeRequest calldata request,
+        ISocketRegistry.BridgeRequest calldata expectedRequest
+    ) public pure {
+        require(request.id == expectedRequest.id, CallDataVerifyErrors.INVALID_BRIDGE_ID);
         require(
             request.optionalNativeAmount == expectedRequest.optionalNativeAmount,
-            "bridge optionalNativeAmount invalid"
+            CallDataVerifyErrors.INVALID_BRIDGE_AMOUNT
         );
-        require(request.inputToken == expectedRequest.inputToken, "bridge inputToken invalid");
-        if (expectedRequest.data.length > 0) {
-            require(keccak256(request.data) == keccak256(expectedRequest.data), "bridge data invalid");
-        }
+        require(request.inputToken == expectedRequest.inputToken, CallDataVerifyErrors.INVALID_BRIDGE_TOKEN);
     }
 
     /// @notice Verify the middleware request if it is specified
     /// @param request Middleware request
     /// @param expectedRequest Middleware request that is expected
-    function verifyMiddlewareRequest(MiddlewareRequest calldata request, MiddlewareRequest calldata expectedRequest)
-        public
-        pure
-    {
+    function verifyMiddlewareRequest(
+        ISocketRegistry.MiddlewareRequest calldata request,
+        ISocketRegistry.MiddlewareRequest calldata expectedRequest
+    ) public pure {
         if (request.id == 0) {
             // If middleware request ID is 0, middleware verification is not required
             return;
         }
-        require(request.id == expectedRequest.id, "middleware id invalid");
+        require(request.id == expectedRequest.id, CallDataVerifyErrors.INVALID_MIDDLEWARE_ID);
         require(
             request.optionalNativeAmount == expectedRequest.optionalNativeAmount,
-            "middleware optionalNativeAmount invalid"
+            CallDataVerifyErrors.INVALID_MIDDLEWARE_AMOUNT
         );
-        require(request.inputToken == expectedRequest.inputToken, "middleware inputToken invalid");
-        if (expectedRequest.data.length > 0) {
-            require(keccak256(request.data) == keccak256(expectedRequest.data), "middleware data invalid");
-        }
+        require(request.inputToken == expectedRequest.inputToken, CallDataVerifyErrors.INVALID_MIDDLEWARE_TOKEN);
     }
 
     /// @notice Verify the calldata to V2 with a given expected UserRequest
     /// @param data Socket V2 call data
     /// @param expected Middleware request that is expected
-    function verifyCallData(bytes calldata data, UserRequest calldata expected) public view {
-        UserRequest memory userRequest = decodeCallData(data);
-        require(userRequest.toChainId == expected.toChainId, "invalid toChainId");
-        require(userRequest.receiverAddress == expected.receiverAddress, "invalid receiverAddress");
-        require(userRequest.amount == expected.amount, "invalid amount");
+    function verifyCallData(bytes calldata data, ISocketRegistry.UserRequest calldata expected) public view {
+        ISocketRegistry.UserRequest memory userRequest = decodeCallData(data);
+        require(userRequest.toChainId == expected.toChainId, CallDataVerifyErrors.INVALID_TO_CHAIN_ID);
+        require(userRequest.receiverAddress == expected.receiverAddress, CallDataVerifyErrors.INVALID_RECEIVER_ADDRESS);
+        require(userRequest.amount == expected.amount, CallDataVerifyErrors.INVALID_AMOUNT);
         this.verifyBridgeRequest(userRequest.bridgeRequest, expected.bridgeRequest);
         this.verifyMiddlewareRequest(userRequest.middlewareRequest, expected.middlewareRequest);
+    }
+
+    function verifyApprovalData(
+        bytes calldata data,
+        address registryAddress,
+        uint256 routeId,
+        uint256 expectedAmount
+    ) public view {
+        (address spender, uint256 amount) = decodeApprovalData(data);
+        ISocketRegistry.RouteData memory route = ISocketRegistry(registryAddress).routes()[routeId];
+        require(route.isEnabled, ApprovalVerifyErrors.INVALID_ROUTE);
+        require(route.route == spender, ApprovalVerifyErrors.INVALID_SPENDER);
+        require(amount == expectedAmount, ApprovalVerifyErrors.INVALID_AMOUNT);
     }
 }
---
+++ contracts/helpers/errors.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.4;
+
+library CallDataVerifyErrors {
+    string internal constant INVALID_TO_CHAIN_ID = "INVALID_TO_CHAIN_ID";
+    string internal constant INVALID_RECEIVER_ADDRESS = "INVALID_RECEIVER_ADDRESS";
+    string internal constant INVALID_AMOUNT = "INVALID_AMOUNT";
+
+    string internal constant INVALID_BRIDGE_ID = "INVALID_BRIDGE_ID";
+    string internal constant INVALID_BRIDGE_AMOUNT = "INVALID_BRIDGE_AMOUNT";
+    string internal constant INVALID_BRIDGE_TOKEN = "INVALID_BRIDGE_TOKEN";
+
+    string internal constant INVALID_MIDDLEWARE_ID = "INVALID_MIDDLEWARE_ID";
+    string internal constant INVALID_MIDDLEWARE_AMOUNT = "INVALID_MIDDLEWARE_AMOUNT";
+    string internal constant INVALID_MIDDLEWARE_TOKEN = "INVALID_MIDDLEWARE_TOKEN";
+}
+
+library ApprovalVerifyErrors {
+    string internal constant INVALID_ROUTE = "INVALID_ROUTE";
+    string internal constant INVALID_SPENDER = "INVALID_SPENDER";
+    string internal constant INVALID_AMOUNT = "INVALID_AMOUNT";
+}
