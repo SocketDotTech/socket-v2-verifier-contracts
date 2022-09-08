0382231ca948f2170740ce967dabe1764e48fcf5
rugamoto <apetoshi.rugamoto@gmail.com>
2022-09-08 10:44:59 +0100

    refactor: change order of functions

--- contracts/SocketV2Verifier.sol
+++ contracts/SocketV2Verifier.sol
@@ -6,23 +6,34 @@ import "./interfaces/ISocketRegistry.sol";
 import "./helpers/errors.sol";
 
 contract SocketV2Verifier {
-    /// @notice Decode the socket v2 calldata
-    /// @param data Socket V2 outboundTransferTo call data
-    /// @return userRequest socket UserRequest
-    function decodeCallData(bytes calldata data)
-        internal
-        pure
-        returns (ISocketRegistry.UserRequest memory userRequest)
-    {
-        (userRequest) = abi.decode(data[4:], (ISocketRegistry.UserRequest));
+    /// @notice Verify the calldata to V2 with a given expected UserRequest
+    /// @param data Socket V2 call data
+    /// @param expected Middleware request that is expected
+    function verifyCallData(bytes calldata data, ISocketRegistry.UserRequest calldata expected) public view {
+        ISocketRegistry.UserRequest memory userRequest = decodeCallData(data);
+        require(userRequest.toChainId == expected.toChainId, CallDataVerifyErrors.INVALID_TO_CHAIN_ID);
+        require(userRequest.receiverAddress == expected.receiverAddress, CallDataVerifyErrors.INVALID_RECEIVER_ADDRESS);
+        require(userRequest.amount == expected.amount, CallDataVerifyErrors.INVALID_AMOUNT);
+        this.verifyBridgeRequest(userRequest.bridgeRequest, expected.bridgeRequest);
+        this.verifyMiddlewareRequest(userRequest.middlewareRequest, expected.middlewareRequest);
     }
 
-    /// @notice Decode erc20 approval call
-    /// @param data ERC20 apporval calldata
-    /// @return spender erc20 spender
-    /// @return amount erc20 amount
-    function decodeApprovalData(bytes calldata data) internal pure returns (address spender, uint256 amount) {
-        (spender, amount) = abi.decode(data[4:], (address, uint256));
+    /// @notice Verify the approval call data
+    /// @param data Approval call data
+    /// @param registryAddress The address of the socket registry
+    /// @param routeId The expected route id
+    /// @param expectedAmount The expected amount
+    function verifyApprovalData(
+        bytes calldata data,
+        address registryAddress,
+        uint256 routeId,
+        uint256 expectedAmount
+    ) public view {
+        (address spender, uint256 amount) = decodeApprovalData(data);
+        ISocketRegistry.RouteData memory route = ISocketRegistry(registryAddress).routes()[routeId];
+        require(route.isEnabled, ApprovalVerifyErrors.INVALID_ROUTE);
+        require(route.route == spender, ApprovalVerifyErrors.INVALID_SPENDER);
+        require(amount == expectedAmount, ApprovalVerifyErrors.INVALID_AMOUNT);
     }
 
     /// @notice Verify the bridge request
@@ -59,33 +70,22 @@ contract SocketV2Verifier {
         require(request.inputToken == expectedRequest.inputToken, CallDataVerifyErrors.INVALID_MIDDLEWARE_TOKEN);
     }
 
-    /// @notice Verify the calldata to V2 with a given expected UserRequest
-    /// @param data Socket V2 call data
-    /// @param expected Middleware request that is expected
-    function verifyCallData(bytes calldata data, ISocketRegistry.UserRequest calldata expected) public view {
-        ISocketRegistry.UserRequest memory userRequest = decodeCallData(data);
-        require(userRequest.toChainId == expected.toChainId, CallDataVerifyErrors.INVALID_TO_CHAIN_ID);
-        require(userRequest.receiverAddress == expected.receiverAddress, CallDataVerifyErrors.INVALID_RECEIVER_ADDRESS);
-        require(userRequest.amount == expected.amount, CallDataVerifyErrors.INVALID_AMOUNT);
-        this.verifyBridgeRequest(userRequest.bridgeRequest, expected.bridgeRequest);
-        this.verifyMiddlewareRequest(userRequest.middlewareRequest, expected.middlewareRequest);
+    /// @notice Decode the socket v2 calldata
+    /// @param data Socket V2 outboundTransferTo call data
+    /// @return userRequest socket UserRequest
+    function decodeCallData(bytes calldata data)
+        internal
+        pure
+        returns (ISocketRegistry.UserRequest memory userRequest)
+    {
+        (userRequest) = abi.decode(data[4:], (ISocketRegistry.UserRequest));
     }
 
-    /// @notice Verify the approval call data
-    /// @param data Approval call data
-    /// @param registryAddress The address of the socket registry
-    /// @param routeId The expected route id
-    /// @param expectedAmount The expected amount
-    function verifyApprovalData(
-        bytes calldata data,
-        address registryAddress,
-        uint256 routeId,
-        uint256 expectedAmount
-    ) public view {
-        (address spender, uint256 amount) = decodeApprovalData(data);
-        ISocketRegistry.RouteData memory route = ISocketRegistry(registryAddress).routes()[routeId];
-        require(route.isEnabled, ApprovalVerifyErrors.INVALID_ROUTE);
-        require(route.route == spender, ApprovalVerifyErrors.INVALID_SPENDER);
-        require(amount == expectedAmount, ApprovalVerifyErrors.INVALID_AMOUNT);
+    /// @notice Decode erc20 approval call
+    /// @param data ERC20 apporval calldata
+    /// @return spender erc20 spender
+    /// @return amount erc20 amount
+    function decodeApprovalData(bytes calldata data) internal pure returns (address spender, uint256 amount) {
+        (spender, amount) = abi.decode(data[4:], (address, uint256));
     }
 }
